183bb4e5eed3fccd8fa97ccdab2cd0e7668501b8
Adnan_shaikh <adnan.sits.mech@gmail.com>
2023-05-16 09:41:39 +0530

    second commit

---
+++ todo-app/components/Header.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header class="text-gray-600 body-font">
+                <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <Link href={'/'} class="ml-0 text-black text-xl">MY TODOS</Link>
+                    </a>
+                    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                        <Link href={'/my-todos'} class="mr-5 hover:text-gray-900">MY TODOS</Link>
+                        <Link href={'/'} class="mr-5 hover:text-gray-900">HOME</Link>
+                        <Link href={'/'} class="mr-5 hover:text-gray-900">ABOUT</Link>
+                        <Link href={'/'} class="mr-5 hover:text-gray-900">FEEDBACK</Link>
+                    </nav>
+
+                </div>
+            </header>
+        </div>
+    )
+}
+export default Header
