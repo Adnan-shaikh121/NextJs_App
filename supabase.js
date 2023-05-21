5864cb0b4f68d4c9ff080e58c1acd3304d2390df
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-05-21 10:42:27 +0530

    other table added

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bnyfcftmqqiqrwyrgpyy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJueWZjZnRtcXFpcXJ3eXJncHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MDMzNjUsImV4cCI6MTk5OTk3OTM2NX0.0J_WAVgHLczCh1cyXfcFNv2_NPrG5_OmbUtEyBECPmc';
+const supabaseUrl = 'https://omaxhdylcmspuwfssvee.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tYXhoZHlsY21zcHV3ZnNzdmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0Mzg3OTcsImV4cCI6MTk5ODAxNDc5N30.RUYrx4K1SIlrm4gLCceIliZpN37sHbeK8o5FCG9iNJo';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
